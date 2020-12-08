c6a26defcae2211694b84c3c94358f14dfbf3db4
macle <ysys86a@gmail.com>
2020-12-08 15:24:24 +0900

    시간에 대한 의존성 업데이트

--- src/test/java/com/wigoai/nipa/regional/service/restcall/v1/IntegratedAnalysisCall.java
+++ src/test/java/com/wigoai/nipa/regional/service/restcall/v1/IntegratedAnalysisCall.java
@@ -20,9 +20,14 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
+import com.seomse.commons.utils.time.Times;
 import com.wigoai.rest.RestCall;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.moara.common.config.Config;
 
 import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * 통합분석
@@ -34,10 +39,19 @@ public class IntegratedAnalysisCall {
         long analysisStartTime = System.currentTimeMillis();
 
         //7월 20일부터
-        long startTime = new SimpleDateFormat("yyyyMMdd HH:mm:ss").parse("20200720 00:00:00").getTime();
+        long startTime = new SimpleDateFormat("yyyyMMdd HH:mm:ss").parse("20201202 00:00:00").getTime();
 
         //7월 25일 전까지 (7월24일까지)
-        long endTime = new SimpleDateFormat("yyyyMMdd HH:mm:ss").parse("20200730 00:00:00").getTime();
+
+        System.out.println(Times.DAY_1 -1);
+
+        //전일자
+        long endTime = new SimpleDateFormat("yyyyMMdd HH:mm:ss").parse("20201202 00:00:00").getTime() + (Times.DAY_1 -1);
+
+
+
+        //오늘포함
+        // endTime = System.currentTimeMillis();
 
         long standardTime = System.currentTimeMillis();
 
@@ -54,13 +68,23 @@ public class IntegratedAnalysisCall {
 
         JsonArray keywords = new JsonArray();
 
-        keywords.add("춘천");
+        keywords.add("속초");
         param.add("keywords", keywords);
 
 
         String request = gson.toJson(param);
         String responseMessage = RestCall.postJson("http://127.0.0.1:33377/nipars/v1/integrated/analysis",request);
-//        String responseMessage = RestCall.postJson("http://sc.wigo.ai:10015/nipars/v1/integrated/analysis",request);
+//        String responseMessage = RestCall.postJson("http://sc.wigo.ai:10014/nipars/v1/integrated/analysis",request);
+
+        JSONObject check = new JSONObject(responseMessage);
+
+        JSONArray array = check.getJSONObject("channel_count").getJSONArray("time_tf_arrays");
+
+        for (int i = 0; i <array.length() ; i++) {
+
+            System.out.println("ymd: " +new SimpleDateFormat(Config.DATE_FORMAT_JAVA).format(new Date(array.getJSONObject(i).getLong("time"))));
+
+        }
 
 
         System.out.println("mills second: " + (System.currentTimeMillis() - analysisStartTime));
