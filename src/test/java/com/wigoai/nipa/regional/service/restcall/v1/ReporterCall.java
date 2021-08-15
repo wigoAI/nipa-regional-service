f4ea4a0c8ae63e3087f6c8a6ab22ab3ff477f665
macle86 <ysys86a@gmail.com>
2021-08-15 09:25:15 +0900

    매체별 기자분석

--- src/test/java/com/wigoai/nipa/regional/service/restcall/v1/ReporterCall.java
+++ src/test/java/com/wigoai/nipa/regional/service/restcall/v1/ReporterCall.java
@@ -10,18 +10,17 @@ import com.wigoai.rest.RestCall;
 import java.text.SimpleDateFormat;
 
 /**
- * 인물별분석 현황호출 예제
+ * 기자별 현황호출 예제
  * @author macle
  */
-public class IntegratedAnalysisCall {
-
+public class ReporterCall {
     public static void main(String[] args) throws Exception {
         long analysisStartTime = System.currentTimeMillis();
+
 
         long startTime = new SimpleDateFormat("yyyyMMdd HH:mm:ss").parse("20190101 00:00:00").getTime();
 
         long endTime = new SimpleDateFormat("yyyyMMdd HH:mm:ss").parse("20210815 00:00:00").getTime() + (Times.DAY_1 -1);
-
 
         //오늘포함
         // endTime = System.currentTimeMillis();
@@ -34,22 +33,18 @@ public class IntegratedAnalysisCall {
         param.addProperty("start_time", startTime);
         param.addProperty("end_time", endTime);
         param.addProperty("standard_time", standardTime);
-
+        param.addProperty("channel_id", "bfly_es_news_063");
         //단어 최대 건수설정 설정하지 않으면 30
 //        param.addProperty("keyword_count", 30);
-
-
         JsonArray keywords = new JsonArray();
-
         keywords.add("코로나");
-
-
+        keywords.add("강원도");
+        keywords.add("춘천");
         param.add("keywords", keywords);
-
 
         String request = gson.toJson(param);
 //        String responseMessage = RestCall.postJson("http://127.0.0.1:33377/nipars/v1/integrated/analysis",request);
-        String responseMessage = RestCall.postJson("http://sc.wigo.ai:10015/nipars/v1/integrated/analysis",request);
+        String responseMessage = RestCall.postJson("http://sc.wigo.ai:10015/nipars/v1/media/reporter",request);
 
         System.out.println("mills second: " + (System.currentTimeMillis() - analysisStartTime));
         System.out.println("request\n " + request +"\n");
--- src/test/java/com/wigoai/nipa/regional/service/restcall/v1/ReporterCharacterCall.java
+++ src/test/java/com/wigoai/nipa/regional/service/restcall/v1/ReporterCharacterCall.java
@@ -8,25 +8,24 @@ import com.seomse.commons.utils.time.Times;
 import com.wigoai.rest.RestCall;
 
 import java.text.SimpleDateFormat;
-
 /**
- * 인물별분석 현황호출 예제
+ * 인물분석 기자별 현황
  * @author macle
  */
-public class IntegratedAnalysisCall {
-
+public class ReporterCharacterCall {
     public static void main(String[] args) throws Exception {
         long analysisStartTime = System.currentTimeMillis();
+
 
         long startTime = new SimpleDateFormat("yyyyMMdd HH:mm:ss").parse("20190101 00:00:00").getTime();
 
         long endTime = new SimpleDateFormat("yyyyMMdd HH:mm:ss").parse("20210815 00:00:00").getTime() + (Times.DAY_1 -1);
 
-
         //오늘포함
         // endTime = System.currentTimeMillis();
 
         long standardTime = System.currentTimeMillis();
+
 
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
@@ -34,22 +33,18 @@ public class IntegratedAnalysisCall {
         param.addProperty("start_time", startTime);
         param.addProperty("end_time", endTime);
         param.addProperty("standard_time", standardTime);
-
-        //단어 최대 건수설정 설정하지 않으면 30
-//        param.addProperty("keyword_count", 30);
-
-
-        JsonArray keywords = new JsonArray();
-
-        keywords.add("코로나");
+        param.addProperty("search_type", "FILTER");
+        param.addProperty("channel_id", "bfly_es_news_063");
 
 
-        param.add("keywords", keywords);
+        param.addProperty("name","문재인");
+        JsonArray infos = new JsonArray();
+        infos.add("코로나");
 
+        param.add("infos", infos);
 
         String request = gson.toJson(param);
-//        String responseMessage = RestCall.postJson("http://127.0.0.1:33377/nipars/v1/integrated/analysis",request);
-        String responseMessage = RestCall.postJson("http://sc.wigo.ai:10015/nipars/v1/integrated/analysis",request);
+        String responseMessage = RestCall.postJson("http://sc.wigo.ai:10015/nipars/v1/character/reporter",request);
 
         System.out.println("mills second: " + (System.currentTimeMillis() - analysisStartTime));
         System.out.println("request\n " + request +"\n");
